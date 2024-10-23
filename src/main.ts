58a57caf8644d9e7c52068d6ee55b12c2815e23e
brian feng <42507580+brian-feng@users.noreply.github.com>
2024-10-23 20:17:14 +0000

    Step 2

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,34 @@
 import "./style.css";
 
-const APP_NAME = "Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
-document.title = APP_NAME;
+document.title = "Sketchpad";
 app.innerHTML = "<h1>Sketchpad</h1>";
 
 const canvas = document.createElement("canvas");
-canvas.style.width = "256px";
-canvas.style.height = "256px";
+canvas.height = 256;
+canvas.width = 256;
 app.appendChild(canvas);
+
+const context = canvas.getContext("2d");
+const cursor = {active: false, x: 0, y: 0};
+canvas.addEventListener("mousedown", (e) => {
+    cursor.active = true;
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if(cursor.active && context){
+        context.beginPath()
+        context.moveTo(cursor.x, cursor.y);
+        context.lineTo(e.offsetX, e.offsetY);
+        context.stroke();
+        cursor.x = e.offsetX;
+        cursor.y = e.offsetY;
+    }
+});
+
+canvas.addEventListener("mouseup", (e) => {
+    cursor.active = false;
+});
