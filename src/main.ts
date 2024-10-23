ee572ea01091e42b9da33894f1540fb5076e24bd
brian feng <42507580+brian-feng@users.noreply.github.com>
2024-10-23 19:48:37 +0000

    First change

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+app.innerHTML = "wadup nerds";
